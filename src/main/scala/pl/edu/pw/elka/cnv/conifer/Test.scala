509d3089a79ad03326eec8b9b030b607e4a82ce7
Mariusz Kopacz <mariusz.kopacz90@gmail.com>
2015-07-17 17:12:19 +0200

    adjust test to new features

--- src/main/scala/pl/edu/pw/elka/cnv/conifer/Test.scala
+++ src/main/scala/pl/edu/pw/elka/cnv/conifer/Test.scala
@@ -14,20 +14,30 @@ object Test {
       .set("spark.driver.host", "127.0.0.1")
     val sc = new SparkContext(conf)
 
-    val bedFilePath = "/Users/mariusz-macbook/IdeaProjects/zsi-bio-cnv/resources/data/probes.txt"
-    val bamFilesPath = "/Users/mariusz-macbook/IdeaProjects/zsi-bio-cnv/resources/data"
+    val bedFilePath = "/Users/mariusz-macbook/Downloads/ZSI-Bio/Tools/conifer_v0.2.2/exomes_data/20120518.consensus.annotation.txt"
+    val bamFilesPath = "/Users/mariusz-macbook/Downloads/ZSI-Bio/Tools/conifer_v0.2.2/exomes_data"
 
     val conifer = new Conifer(sc, bedFilePath, bamFilesPath)
 
     saveBedFile(conifer.bedFile.sortByKey())
     saveCoverage(conifer.coverage.sortByKey())
+    saveRpkms(conifer.rpkms.sortByKey())
+    saveZrpkms(conifer.zrpkms.sortByKey())
 
-    //    saveRPKMs(conifer.RPKMsByExone)
-    //    saveZRPKMs(conifer.ZRPKMsByExone)
-    //    conifer.getSVD("11")
+    val mat = conifer.calculateSVD
+
+    System.console().printf("Samples: " + conifer.samples.size + "\n")
+    System.console().printf("Reads: " + conifer.reads.count + "\n")
+    System.console().printf("BED: " + conifer.bedFile.count + "\n")
+    System.console().printf("Coverage: " + conifer.coverage.count + "\n")
+    System.console().printf("RPKMS: " + conifer.rpkms.count + "\n")
+    System.console().printf("ZRPKMS: " + conifer.zrpkms.count + "\n")
+    System.console().printf("Matrix rows: " + mat.numRows + "\n")
+    System.console().printf("Matrix cols: " + mat.numCols + "\n")
+
   }
 
-  private def saveBedFile(bedFile: RDD[(Int, (String, Int, Int))]) = {
+  private def saveBedFile(bedFile: RDD[(Int, (Int, Int, Int))]) = {
     val path = "/Users/mariusz-macbook/IdeaProjects/zsi-bio-cnv/resources/results/bedFile.txt"
     maybeRemoveDir(path)
     bedFile.saveAsTextFile(path)
@@ -39,13 +49,13 @@ object Test {
     coverage.saveAsTextFile(path)
   }
 
-  private def saveRPKMs(rpkms: RDD[(Long, Iterable[Double])]) = {
+  private def saveRpkms(rpkms: RDD[(Int, Iterable[(Int, Double)])]) = {
     val path = "/Users/mariusz-macbook/IdeaProjects/zsi-bio-cnv/resources/results/rpkms.txt"
     maybeRemoveDir(path)
     rpkms.saveAsTextFile(path)
   }
 
-  private def saveZRPKMs(zrpkms: RDD[(Long, Iterable[Double])]) = {
+  private def saveZrpkms(zrpkms: RDD[(Int, Iterable[(Int, Double)])]) = {
     val path = "/Users/mariusz-macbook/IdeaProjects/zsi-bio-cnv/resources/results/zrpkms.txt"
     maybeRemoveDir(path)
     zrpkms.saveAsTextFile(path)
