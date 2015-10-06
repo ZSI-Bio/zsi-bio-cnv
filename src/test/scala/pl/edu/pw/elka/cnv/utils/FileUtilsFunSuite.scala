d6c9b1110a2dd089d6e414503e4a56425812f37c
Mariusz Kopacz <mariusz.kopacz90@gmail.com>
2015-10-06 21:34:19 +0200

    add tests to all util methods

--- src/test/scala/pl/edu/pw/elka/cnv/utils/FileUtilsFunSuite.scala
+++ src/test/scala/pl/edu/pw/elka/cnv/utils/FileUtilsFunSuite.scala
@@ -1,5 +1,7 @@
 package pl.edu.pw.elka.cnv.utils
 
+import java.io.Serializable
+
 import org.scalatest.Matchers
 import pl.edu.pw.elka.cnv.SparkFunSuite
 
@@ -8,6 +10,48 @@ import pl.edu.pw.elka.cnv.SparkFunSuite
  */
 class FileUtilsFunSuite extends SparkFunSuite with Matchers {
 
-  //TODO
+  val files = new FileUtils with Serializable
+
+  test("scanForSamples test") {
+    val samples = files.scanForSamples("resources/data")
+
+    samples.keys should have size (1)
+    samples.keys should contain theSameElementsAs Array(0)
+
+    samples(0) should be("resources/data/test_bam_file.bam")
+  }
+
+  sparkTest("loadReads test") {
+    val samples = Map(0 -> "resources/data/test_bam_file.bam")
+    val reads = files.loadReads(sc, samples).collect
+
+    reads should have size (385721)
+
+    reads(0)._1 should be(0)
+    reads(0)._2.getAlignmentStart should be(809713)
+    reads(0)._2.getAlignmentEnd should be(809812)
+
+    reads(192860)._1 should be(0)
+    reads(192860)._2.getAlignmentStart should be(21026808)
+    reads(192860)._2.getAlignmentEnd should be(21026907)
+
+    reads(385720)._1 should be(0)
+    reads(385720)._2.getAlignmentStart should be(90251)
+    reads(385720)._2.getAlignmentEnd should be(90353)
+  }
+
+  test("readBedFile test") {
+    val bedFile = files.readBedFile("resources/data/test_bed_file.txt")
+
+    bedFile.keys should have size (223378)
+    bedFile.keys.min should be(0)
+    bedFile.keys.max should be(223377)
+
+    bedFile(0) should be(1, 69091, 69290)
+    bedFile(55844) should be(12, 104378527, 104378698)
+    bedFile(111688) should be(19, 42471401, 42471492)
+    bedFile(167533) should be(5, 14366969, 14367088)
+    bedFile(223377) should be(24, 27770601, 27770674)
+  }
 
 }
