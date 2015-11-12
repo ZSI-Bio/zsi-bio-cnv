6a34cf862b90e1d36ff7710936ff1cf4e5903317
Mariusz Kopacz <mariusz.kopacz90@gmail.com>
2015-11-12 21:57:21 +0100

    fix parsing cigar string in CoverageCounter

--- src/main/scala/pl/edu/pw/elka/cnv/coverage/CoverageCounter.scala
+++ src/main/scala/pl/edu/pw/elka/cnv/coverage/CoverageCounter.scala
@@ -44,12 +44,12 @@ class CoverageCounter(@transient sc: SparkContext, bedFile: Broadcast[mutable.Ha
       for ((sampleId, read) <- partition)
         if (chromosomesMap.value.contains(read.getReferenceName)) {
           val regions = chromosomesMap.value(read.getReferenceName)
-          val bases = genBases(read)
-          for ((baseStart, baseEnd) <- bases) {
-            val regionsToCheck = getRegionsToCheck(baseStart, regions)
+          val blocks = generateBlocks(read)
+          for ((blockStart, blockEnd) <- blocks) {
+            val regionsToCheck = getRegionsToCheck(blockStart, regions)
             if (regionsToCheck != null)
               for ((regionId, regionStart, regionEnd) <- regionsToCheck)
-                if (countingCondition(baseStart, baseEnd, regionStart, regionEnd)) {
+                if (countingCondition(blockStart, blockEnd, regionStart, regionEnd)) {
                   val coverageId = encodeCoverageId(sampleId, regionId)
                   if (!regionsCountMap.contains(coverageId))
                     regionsCountMap(coverageId) = 1
@@ -71,18 +71,18 @@ class CoverageCounter(@transient sc: SparkContext, bedFile: Broadcast[mutable.Ha
    * @param regionEnd Starting position of a given region.
    * @return Boolean flag.
    */
-  private def countingCondition(baseStart: Int, baseEnd: Int, regionStart: Int, regionEnd: Int): Boolean =
+  private def countingCondition(blockStart: Int, blockEnd: Int, regionStart: Int, regionEnd: Int): Boolean =
     countingMode match {
       case CountingMode.COUNT_WHEN_WHITIN =>
-        if (baseStart >= regionStart && baseEnd <= regionEnd) true
+        if (blockStart >= regionStart && blockEnd <= regionEnd) true
         else false
       case CountingMode.COUNT_WHEN_OVERLAPS =>
-        if ((baseStart >= regionStart && baseStart <= regionEnd)
-          || (baseStart <= regionStart && baseEnd >= regionEnd)
-          || (baseEnd >= regionStart && baseEnd <= regionEnd)) true
+        if ((blockStart >= regionStart && blockStart <= regionEnd)
+          || (blockStart <= regionStart && blockEnd >= regionEnd)
+          || (blockEnd >= regionStart && blockEnd <= regionEnd)) true
         else false
       case CountingMode.COUNT_WHEN_STARTS =>
-        if (baseStart >= regionStart && baseStart <= regionEnd) true
+        if (blockStart >= regionStart && blockStart <= regionEnd) true
         else false
     }
 
@@ -97,10 +97,9 @@ class CoverageCounter(@transient sc: SparkContext, bedFile: Broadcast[mutable.Ha
     val startId = baseStart / 10000
     var result = regions(startId)
 
-    if (startId > 0 && result != null && regions(startId - 1) != null)
-      result = result ++ (regions(startId - 1))
-    else if (startId > 0 && result == null && regions(startId - 1) != null)
-      result = regions(startId - 1)
+    if (startId > 0 && regions(startId - 1) != null)
+      if (result != null) result = result ++ regions(startId - 1)
+      else result = regions(startId - 1)
 
     result
   }
@@ -111,9 +110,9 @@ class CoverageCounter(@transient sc: SparkContext, bedFile: Broadcast[mutable.Ha
    * @param read Read to be analyzed.
    * @return Array of (baseStart, baseEnd) containing all of the bases generated from a given read.
    */
-  private def genBases(read: CNVRecord): ArrayBuffer[(Int, Int)] =
-    if (parseCigar) genBasesFromCigar(read.getAlignmentStart, read.getCigar)
-    else ArrayBuffer((read.getAlignmentStart, read.getAlignmentEnd))
+  private def generateBlocks(read: CNVRecord): Array[(Int, Int)] =
+    if (parseCigar) genBasesFromCigar(read)
+    else Array((read.getAlignmentStart, read.getAlignmentEnd))
 
   /**
    * Method generating bases by parsing a cigar string.
@@ -122,25 +121,9 @@ class CoverageCounter(@transient sc: SparkContext, bedFile: Broadcast[mutable.Ha
    * @param cigar Cigar string of a read.
    * @return Array of (baseStart, baseEnd) containing all of the bases generated from a given cigar string.
    */
-  private def genBasesFromCigar(alignStart: Int, cigar: htsjdk.samtools.Cigar): ArrayBuffer[(Int, Int)] = {
-    val result = new ArrayBuffer[(Int, Int)]
-    val numCigElem = cigar.numCigarElements
-
-    var shift = 0
-    for (i <- 0 until numCigElem) {
-      val cigElem = cigar.getCigarElement(i)
-      if ((i == 0 && cigElem.getOperator.toString == "M") || (i == 1 && cigar.getCigarElement(0).getOperator.toString == "S"))
-        result += ((alignStart, alignStart + cigElem.getLength))
-      else if (cigElem.getOperator.toString != "M")
-        shift += cigElem.getLength
-      else if (i > 1 && result.length > 0 && cigElem.getOperator.toString == "M") {
-        val temp = result.last._2 + shift
-        result += ((temp, temp + cigElem.getLength))
-        shift = 0
-      }
+  private def genBasesFromCigar(read: CNVRecord): Array[(Int, Int)] =
+    read.getAlignmentBlocks map {
+      block => (block.getReferenceStart, block.getReferenceStart + block.getLength - 1)
     }
-
-    result
-  }
 
 }
